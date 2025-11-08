99ee0d88f1c93670383ba885013a6bd292d289ae
BogdanZ19 <bogdanzainia1@gmail.com>
2025-11-08 21:31:50 +0200

    finish print functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_letter_dict
+from stats import get_word_count, get_letter_dict, get_sorted_letter_arr
 
 
 def get_book_text(file_path):
@@ -10,10 +10,19 @@ def main():
     book = "books/frankenstein.txt"
     text = get_book_text(book)
     word_count = get_word_count(text)
-    letters_dict = get_letter_dict(text)
+    letter_dict = get_letter_dict(text)
+    sorted_letter_list = get_sorted_letter_arr(letter_dict)
     # print(text)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    print(letters_dict)
+    print("--------- Character Count -------")
+    for letter in sorted_letter_list:
+        if letter["char"].isalpha() is True:
+            print (f"{letter["char"]}: {letter["num"]}")
+    
 main()
     
     
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ def get_word_count(book_text):
     arr = book_text.split()
     return len(arr)
 
+
 def get_letter_dict(book_text):
     letter_dict = {}
     for letter in book_text:
@@ -9,6 +10,18 @@ def get_letter_dict(book_text):
         if letter in letter_dict:
             letter_dict[letter] += 1
         else:
-            letter_dict[letter] = 1            
-    return letter_dict 
-        
+            letter_dict[letter] = 1
+    return letter_dict
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def get_sorted_letter_arr(letter_dict):
+    dict_list = []
+    
+    for letter in letter_dict:
+        dict_list.append({"char": letter, "num": letter_dict[letter]})
+    dict_list.sort(reverse = True, key=sort_on)   
+    return dict_list
